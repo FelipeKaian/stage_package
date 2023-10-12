a06bbc9000a89529d8fa1edecb416b2fd8eed2c3
FelipeKaian <felipekaianmutti@gmail.com>
2023-10-12 18:19:38 -0300

    publish setup

--- test/stage_package_test.dart
+++ test/stage_package_test.dart
@@ -1,9 +1,5 @@
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:stage_package/stage_package.dart';
-
 void main() {
-  test('adds one to input values', () {
-    expect(true,true);
-  });
+
 }
